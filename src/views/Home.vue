df71f2bf63a4895a29ed6d5032e2700bcac9e02c
Christian Farris <christianfarriscs@gmail.com>
2021-06-29 02:02:35 -0500

    use urql client to fetch data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,35 @@
 <template>
   <div class="home">
     <p>Hello World</p>
+    <p>{{ panda }}</p>
+    <p>{{ data }}</p>
+    <p>{{ loading }}</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import { useQuery } from "@urql/vue";
 
 export default defineComponent({
+  setup() {
+    const result = useQuery({
+      query: `
+        query {
+          page: Page(id: "page") {
+            heading
+            next
+          }
+        }
+      `,
+    });
+
+    return {
+      loading: result.fetching,
+      data: result.data,
+      panda: "Panda",
+    };
+  },
   name: "Home",
   components: {},
 });
