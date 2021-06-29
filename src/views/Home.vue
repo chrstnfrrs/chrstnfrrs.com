bb3859527a392eff5697ac78620ad1d5355caba1
Christian Farris <christianfarriscs@gmail.com>
2021-06-29 01:40:22 -0500

    create vue project

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <p>Hello World</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+  components: {},
+});
+</script>
