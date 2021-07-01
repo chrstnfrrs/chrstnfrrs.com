4964c9d8a194401d68c9d4c5733ff86ad89638c1
Christian Farris <christianfarriscs@gmail.com>
2021-06-30 20:59:43 -0500

    Add new pages to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,33 @@ const routes: Array<RouteRecordRaw> = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/blog",
+    name: "Blog",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Blog.vue"),
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Contact.vue"),
+  },
+  {
+    path: "/blog/:slug",
+    name: "Post",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Post.vue"),
+  },
 ];
 
 const router = createRouter({
